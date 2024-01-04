da90dc6de0478e7a42110bb9f6d8fd93c96d464c
BeatrizCamposAlura <154606910+BeatrizCamposAlura@users.noreply.github.com>
2024-01-04 10:49:55 -0300

    entrega dos exercícios do livro de lógica

---
+++ exercicios_livro_logica.js
@@ -0,0 +1,32 @@
+console.log("Livro - lógica de programação - Casa do código");
+
+/* definiçãotipos de variáveis
+idade inteiro
+altura Real 
+conjunto de caracteres - string*/
+
+//concatenação
+
+var idade = 18;
+var nome = "Beatriz";
+
+console.log(`Tenho ${idade} anos e me chamo ${nome}`);
+
+//operações e concatenação
+
+var numero1 = 12;
+var numero2 = 2;
+var adicao = (numero1 + numero2);
+var subtracao = (numero1 - numero2);
+var multiplicacao = (numero1 * numero2);
+var divisao = (numero1 / numero2);
+
+console.log(`Adição de ${numero1} + ${numero2} é ${adicao} `);
+console.log(`Subtração de ${numero1} - ${numero2} é ${subtracao} `);
+console.log(`Adição de ${numero1} + ${numero2} é ${adicao} ` );
+console.log(`Multiplicação de ${numero1} * ${numero2} é ${multiplicacao} `);
+console.log(`Divisão de ${numero1} / ${numero2} é ${divisao} `);
+
+// condicionais 
+
+
